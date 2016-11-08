aae0e2df2db118a92556c83b3bffcea84a8a6906
gaohailang <ghlndsl@126.com>
2016-11-08 18:08:59 +0800

    feat: 集成 oh-my-zsh install/config etc

---
+++ onmyzsh/config.zsh
@@ -0,0 +1,11 @@
+plugins=(git bundler osx rake ruby zsh-autosuggestions)
+ZSH_THEME="robbyrussell"
+
+
+# Custom Plugins and Themes
+
+# If you want to override any of the default behaviors, just add a new file (ending in .zsh) in the custom/ directory.
+
+# If you have many functions that go well together, you can put them as a XYZ.plugin.zsh file in the custom/plugins/ directory and then enable this plugin.
+
+# If you would like to override the functionality of a plugin distributed with Oh My Zsh, create a plugin of the same name in the custom/plugins/ directory and it will be loaded instead of the one in plugins/.
---
+++ onmyzsh/install.sh
@@ -0,0 +1,4 @@
+
+# 默认目录 - ~/.dotfiles
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
